9ca1306ca197f1e3c5ac3f9412c2b1c31b03f1f1
Rıdvan Güzel <ridvanguzel42@gmail.com>
2023-11-05 14:17:57 +0300

    User Dashboard
    A responsive user dashboard coded using HTML and CSS. Also, used semantic elements, fontawesome icons, and utility class.

---
+++ User Dashboard/dashborad.js
@@ -0,0 +1,7 @@
+//  const form = document.querySelector("form")
+//  fileInput = form.querySelector(".file-input")
+ 
+//  form.addEventListener("click", ()=>{
+//      fileInput.click();
+//  });
+
